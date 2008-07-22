84229d9f782cfb0d0a8ce2c6f708cce56c00b8e2
Norman Clarke <norman@addthree.com>
2008-07-22 12:55:32 -0300

    Implemented image validation and conversion. Added rcov rake task.

--- rails-test/pic_test.rb
+++ rails-test/pic_test.rb
@@ -5,18 +5,37 @@ require File.dirname(__FILE__) + '/pic'
 class PicTest < Test::Unit::TestCase
   
   def setup
-    Pic.has_image_options[:base_path] = RAILS_ROOT + '/tmp'
+    Pic.has_image_options[:base_path] = File.join(RAILS_ROOT, '/tmp')
   end
   
-  def test_valid
-    @pic = Pic.new
+  def teardown
+    FileUtils.rm_rf(File.join(RAILS_ROOT, 'tmp', 'pics'))
+  end
+  
+  def test_should_be_valid
+    image = "/../../test/fixtures/image.jpg"
+    @pic = Pic.new(:name => "test", :image_data => fixture_file_upload(image, "image/jpeg"))
     assert @pic.valid?
   end
-  
+
+  def test_invalid_image_detected
+    image = "/../../test/fixtures/bad_image.jpg"
+    @pic = Pic.new(:name => "test", :image_data => fixture_file_upload(image, "image/jpeg"))
+    assert !@pic.valid?
+  end
+
   def test_create
     image = "/../../test/fixtures/image.jpg"
     @pic = Pic.new(:name => "test", :image_data => fixture_file_upload(image, "image/jpeg"))
     @pic.save!
   end
+
+  def test_create_with_png
+    image = "/../../test/fixtures/image.png"
+    @pic = Pic.new(:name => "test", :image_data => fixture_file_upload(image, "image/png"))
+    assert @pic.valid?
+    @pic.save!
+  end
+
   
 end
