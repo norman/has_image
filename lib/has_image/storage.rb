52be8869bff94d584a288b81468fab33c3c8818e
Norman Clarke <norman@addthree.com>
2008-07-23 17:34:47 -0300

    Worked on documentation.

--- lib/has_image/storage.rb
+++ lib/has_image/storage.rb
@@ -1,24 +1,30 @@
 require 'active_support'
 require 'stringio'
 require 'fileutils'
+require 'zlib'
+
 
 module HasImage  
   
-  class StorageError < StandardError ; end
-  
+  # Filesystem storage for the HasImage gem.
   class Storage
     
     attr_accessor :image_data, :options, :temp_file
 
     class << self
       
-      # From Jamis Buck. http://www.37signals.com/svn/archives2/id_partitioning.php
+      # Stolen from {Jamis Buck}[http://www.37signals.com/svn/archives2/id_partitioning.php].
       def partitioned_path(id, *args)
         ("%08d" % id).scan(/..../) + args
       end
 
+      # Generates an 8-character random file name to use for the image and its
+      # thumbnails. This is done to avoid having files with unfortunate names.
+      # On one of my sites users frequently upload images with Arabic names,
+      # and they end up being hard to manipulate on the command line.
+      # This also helps prevent a possibly undesirable sitation where the
+      # uploaded images have offensive names.
       def random_file_name
-        require 'zlib'
         Zlib.crc32(Time.now.to_s + rand(10e10).to_s).to_s(36)
       end
           
@@ -28,6 +34,9 @@ module HasImage
       @options = options
     end
 
+    # The image data can be anything that inherits from IO. If you pass in an
+    # instance of Tempfile, it will be used directly without being copied to
+    # a new temp file.
     def image_data=(image_data)
       raise StorageError.new if image_data.blank?
       if image_data.is_a?(Tempfile)
@@ -39,6 +48,14 @@ module HasImage
       end
     end
     
+    # A tip of the hat to attachment_fu.
+    alias uploaded_data= image_data=
+    
+    # A tip of the hat to attachment_fu.
+    alias uploaded_data image_data
+    
+    # Invokes the processor to resize the image(s) and the installs them to
+    # the appropriate directory.
     def install_images(id)
       random_name = Storage.random_file_name
       install_main_image(id, random_name)
@@ -47,15 +64,20 @@ module HasImage
     ensure  
       @temp_file.close! if !@temp_file.closed?
     end
-
+    
+    # Gets the full local filesystem path for an image. For example:
+    # /var/sites/example.com/production/public/photos/0000/0001/3eRdh0zs.jpg
     def filesystem_path_for(object, thumbnail = nil)
       File.join(path_for(object.id), file_name_for(object.file_name, thumbnail))
     end
     
+    # Gets the "web" path for an image. For example:
+    # /photos/0000/0001/3eRdh0zs.jpg
     def public_path_for(object, thumbnail = nil)
       filesystem_path_for(object, thumbnail).gsub(options[:base_path], '')
     end
     
+    # Deletes the images and directory that contains them.
     def remove_images(id)
       FileUtils.rm_r path_for(id)
     end
