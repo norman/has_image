1abef66b6647610b3d0c09b24d63c1a035713e20
Norman Clarke <norman@njclarke.com>
2008-07-24 17:12:58 -0300

    Improved fixture organization.

--- test_rails/pic_test.rb
+++ test_rails/pic_test.rb
@@ -12,50 +12,44 @@ class PicTest < Test::Unit::TestCase
   end
   
   def test_should_be_valid
-    image = "/../../test/fixtures/image.jpg"
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/jpeg"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.jpg", "image/jpeg"))
     assert @pic.valid? , "#{@pic.errors.full_messages.to_sentence}"
   end
 
   def test_should_be_too_big
     Pic.has_image_options[:max_size] = 1.kilobyte
-    image = "/../../test/fixtures/image.jpg"
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/jpeg"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.jpg", "image/jpeg"))
     assert !@pic.valid?
   end
 
   def test_should_be_too_small
     Pic.has_image_options[:min_size] = 1.gigabyte
-    image = "/../../test/fixtures/image.jpg"
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/jpeg"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.jpg", "image/jpeg"))
     assert !@pic.valid?
   end
 
   def test_invalid_image_detected
-    image = "/../../test/fixtures/bad_image.jpg"
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/jpeg"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/bad_image.jpg", "image/jpeg"))
     assert !@pic.valid?
   end
 
   def test_create
-    image = "/../../test/fixtures/image.jpg"
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/jpeg"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.jpg", "image/jpeg"))
     assert @pic.save!
   end
 
   def test_create_with_png
-    image = "/../../test/fixtures/image.png"
     Pic.has_image_options[:min_size] = 1
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/png"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.png", "image/png"))
     assert @pic.save!
   end
 
   def test_multiple_calls_to_valid_doesnt_blow_away_temp_image
-    image = "/../../test/fixtures/image.png"
     Pic.has_image_options[:min_size] = 1
-    @pic = Pic.new(:image_data => fixture_file_upload(image, "image/png"))
+    @pic = Pic.new(:image_data => fixture_file_upload("/image.png", "image/png"))
     @pic.valid?
     assert @pic.valid?
   end
-  
+
 end
+
